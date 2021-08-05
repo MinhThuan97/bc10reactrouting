20703c523805247e94dad97c88bc036380aa31ad
MinhThuan97 <minhthuan97.vietnam@gmail.com>
2021-08-05 15:55:50 +0700

    first update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./container/HomeTemplate/HomePage";
+import AboutPage from "./container/HomeTemplate/AboutPage";
+import ListMoviePage from "./container/HomeTemplate/ListMoviePage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import PageNotFound from "./container/PageNotFound";
+import Navbar from "./container/HomeTemplate/_components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        {/* Trang chủ - localhost:3000 */}
+        <Route exact path="/" component={HomePage} />
+
+        {/* Trang about - localhost:3000/about */}
+        <Route path="/about" component={AboutPage} />
+
+        {/* Trang Listmovie - localhost:3000/list-movie */}
+        <Route path="/list-movie" component={ListMoviePage} />
+
+        {/* Trang Không tồn tại */}
+        <Route path="" component={PageNotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
